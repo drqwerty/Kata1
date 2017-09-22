72e7c3cad0c3cb7ca5d6429012534406367b0e40
Dani <Dani@10.225.169.94>
2017-09-22 09:58:19 +0100

    Calendar ahora tiene la fecha correcta.

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -7,7 +7,7 @@ public class Kata1 {
 
     public static void main(String[] args) {
         Calendar date = GregorianCalendar.getInstance();
-        date.set(1994, 9, 7);
+        date.set(1994, 9-1, 7);
         Person person = new Person("Dani", date);
         System.out.println(person.getName() + " tiene " + person.getAge()+ " años");
     }
