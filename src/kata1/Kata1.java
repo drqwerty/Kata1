a913b2fa02418083223d80f8921101392bdc8a18
Dani <Dani@10.225.169.94>
2017-09-22 09:41:24 +0100

    Date cambiado por Calendar

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,12 +1,15 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Dani", new Date(94, 9-1, 7));
-        System.out.println(person.getName() + " tiene " + person.getAge());
+        Calendar date = GregorianCalendar.getInstance();
+        date.set(1994, 9, 7);
+        Person person = new Person("Dani", date);
+        System.out.println(person.getName() + " tiene " + person.getAge()+ " años");
     }
     
 }
